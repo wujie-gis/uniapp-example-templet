863679289fd1c3e0f552afb917b2ec15c708adee
wuj <1450626722@qq.com>
2022-07-14 17:09:13 +0800

    ✨ feat(): vuex

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
-import App from "./App";
-import Vue from "vue";
-import uView from "uni_modules/uview-ui";
+import App from './App';
+import Vue from 'vue';
+import uView from 'uni_modules/uview-ui';
+import store from './store';
 
+Vue.prototype.$store = store;
 Vue.config.productionTip = false;
-App.mpType = "app";
+App.mpType = 'app-example';
 
 Vue.use(uView);
 
 const app = new Vue({
+  store,
   ...App,
 });
+
+// 引入请求封装
+require('./util/request/index')(app);
 app.$mount();
