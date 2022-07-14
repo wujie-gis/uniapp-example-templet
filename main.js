56da48427f2b21f4009eaa3e8ccadfc2ddf7bcee
wuj <1450626722@qq.com>
2022-07-14 16:17:50 +0800

    ✨ feat(): init,uview

---
+++ main.js
@@ -0,0 +1,13 @@
+import App from "./App";
+import Vue from "vue";
+import uView from "uni_modules/uview-ui";
+
+Vue.config.productionTip = false;
+App.mpType = "app";
+
+Vue.use(uView);
+
+const app = new Vue({
+  ...App,
+});
+app.$mount();
